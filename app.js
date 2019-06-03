cde058c53167c911273b0201294d86bd4a6405c6
steevLP <mauricefl@outlook.de>
2019-06-03 22:57:44 +0200

    Updater Fix

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0458fcfc56b7d58856760c5d5bc7fec4389c23958e2ac391e509baeed358dcd4
-size 3978
+oid sha256:9b59a396ec878cb8519d68010da999e0f525680c69c90c280a3a266f49ef5113
+size 3732
